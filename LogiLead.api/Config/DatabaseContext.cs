af0692b4c443bdc430eab9d3bc815584383d8dfb
Bruno Donatelli <bruno.donatelli17@gmail.com>
2023-12-12 19:54:28 -0300

    feat: add dbcontext

---
+++ LogiLead.api/Config/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using LogiLead.api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogiLead.api.Config
+{
+    public static class DatabaseContext
+    {
+        public static void AddContext(WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<LogiLeadContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString(""))
+            );
+        }
+    }
+}
---
+++ LogiLead.api/Data/LogiLeadContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LogiLead.api.Data
+{
+    public class LogiLeadContext : DbContext
+    {
+        public LogiLeadContext(DbContextOptions<LogiLeadContext> options) : base(options)
+        {}
+    }
+}
