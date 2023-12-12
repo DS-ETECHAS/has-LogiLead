56992c9dfc3d52f1831c68a75e1adad1bb906cb5
Bruno Donatelli <bruno.donatelli17@gmail.com>
2023-12-12 19:55:21 -0300

    feat: add models

---
+++ LogiLead.api/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LogiLead.api.Models
+{
+    [Table("User")]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ LogiLead.api/Models/UserData.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace LogiLead.api.Models
+{
+    public class UserData
+    {
+        public int UserId { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        [MaxLength(75)]
+        public string Email { get; set; }
+        
+        [JsonIgnore]
+        public string HashedPassword { get; set; }
+        
+        [JsonIgnore]
+        public string Salt { get; set; }
+        
+        public DateTime PasswordChangeDate { get; set; }
+    }
+}
